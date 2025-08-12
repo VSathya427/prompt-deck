b17f063bae6e4b6c27165d6ceaebf67f0a0e0170
vsathya427 <56964957+VSathya427@users.noreply.github.com>
2025-08-12 14:48:13 -0700

    05-ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,21 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        //download step
-        await step.sleep("wait-a-moment", "30s");
-        //transcript step
-        await step.sleep("wait-a-moment", "10s");
-        //summary step
-        await step.sleep("wait-a-moment", "5s");
+        
+        const codeAgent = createAgent({
+            name: "code-agent",
+            system: "You are a expert next.js developer. You write readable, maintainable, and efficient code. You write simple Next.js and React snippets.",
+            model: gemini({model:"gemini-2.5-pro"}),
+        })
 
+        const { output } = await codeAgent.run(
+            'Write the following snippet: ' + event.data.value,
+        );
 
-        return { message: `Hello ${event.data.email}!` };
+        return { output };
     },
 );
